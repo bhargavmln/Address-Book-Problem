628bc1d3b12720c0c2d4fc29aeb1201698206a80
bhargavmln <bhargavmln@gmail.com>
2020-10-01 22:47:58 +0530

    [bhargavmln] add: Added contact class

---
+++ Contact.java
@@ -0,0 +1,26 @@
+public class Contact{
+	
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNo;
+	private String email;
+	
+	
+	public Contact(String firstName,String lastName,String address,String city, String state, String zip,String phoneNo,String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+		this.city = city;
+		this.state = state;
+        this.zip = zip;
+        this.phoneNo = phoneNo;
+        this.email = email;
+    }
+}
+	
+	
+	
