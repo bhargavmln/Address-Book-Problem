eddd5ab15b0e74c7e87dfb1b1fb063199487ec50
bhargavmln <bhargavmln@gmail.com>
2020-10-02 14:08:26 +0530

    [bhargavmln] add: added a feature to add contact details to Address Book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,8 +1,36 @@
+import java.util.*;
 public class AddressBookMain{
 	
 	public static void main(String[] args){
 		
-	System.out.println("Welcome to Address Book Program");
-	
+		Scanner sc = new Scanner(System.in);
+		//welcome statemment
+		System.out.println("Welcome to Address Book Program");
+		
+		Contact entry = new Contact();
+		List<Contact> addressBook = new ArrayList<Contact>();
+		char selection = 'Y';
+		while(selection =='Y'){
+			System.out.println("Enter First Name: ");
+			entry.setFirstName(sc.next());
+			System.out.println("Enter Last Name:");
+            entry.setLastName(sc.next());
+            System.out.println("Enter  Address:");
+			entry.setAddress(sc.next());
+			System.out.println("Enter the City:");
+			entry.setCity(sc.next());
+			System.out.println("Enter the State:");
+			entry.setState(sc.next());
+            System.out.println("Enter Zip Code:");
+			entry.setZip(sc.next());
+            System.out.println("Enter Phone Number:");
+			entry.setPhoneNo(sc.next());
+            System.out.println("Enter Email Address:");
+			entry.setEmail(sc.next());
+			System.out.println(entry.toString());
+            System.out.println("Add new Contact? (Y/N):");
+            selection = sc.next().charAt(0);
+			addressBook.add(entry);
+		}			
 	}
 }
--- Contact.java
+++ Contact.java
@@ -1,5 +1,6 @@
 public class Contact{
 	
+	//variables
 	private String firstName;
 	private String lastName;
 	private String address;
@@ -9,17 +10,62 @@ public class Contact{
 	private String phoneNo;
 	private String email;
 	
-	
-	public Contact(String firstName,String lastName,String address,String city, String state, String zip,String phoneNo,String email){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
+    
+	//setters
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+		}
+	public void setAddress(String address){
+		this.address = address;
+	}
+	public void setCity(String city){
 		this.city = city;
+	}
+	public void setState(String state){
 		this.state = state;
-        this.zip = zip;
-        this.phoneNo = phoneNo;
-        this.email = email;
-    }
+	}
+	public void setZip(String zip){
+		this.zip = zip;
+	}
+	public void setPhoneNo(String phoneNo){
+		this.phoneNo = phoneNo;
+	}
+	public void setEmail(String email){
+		this.email = email;
+	}
+	//getters
+	public String getFirstName(){
+		return this.firstName;
+	}
+	public String getLastName(){
+		return this.lastName;
+	}
+	public String getAddress(){
+		return this.address;
+	}
+	public String getCity(){
+		return this.city;
+	}
+	public String getState(){
+		return this.state;
+	}
+	public String getZip(){
+		return this.zip;
+	}
+	public String getPhoneNo(){
+		return this.phoneNo;
+	}
+	public String getEmail(){
+		return this.email;
+	}
+	
+	@Override
+	public String toString(){
+		return "The contact details of " + firstName + " " + lastName + " has been added.";
+	}
 }
 	
 	
